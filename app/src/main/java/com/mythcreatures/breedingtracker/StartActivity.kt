826fddb53445db9dbe5b40408f52868d2cc731bc
willdavidking <will.david.king@gmail.com>
2019-08-24 17:02:12 -0600

    refactor/integrating-with-services -) Intergrated Firebase Performance Monitor.

--- app/src/main/java/com/mythcreatures/breedingtracker/StartActivity.kt
+++ app/src/main/java/com/mythcreatures/breedingtracker/StartActivity.kt
@@ -3,6 +3,8 @@ package com.mythcreatures.breedingtracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.perf.FirebasePerformance;
+import com.google.firebase.perf.metrics.Trace;
 
 class StartActivity : AppCompatActivity() {
 
@@ -14,10 +16,16 @@ class StartActivity : AppCompatActivity() {
 
         firebaseAnalytics = FirebaseAnalytics.getInstance(this)
 
-        // TODO Analytics for Testing Purposes, Removed when not needed
+        // TODO Analytics & Performance for Testing Purposes, Removed when not needed
+        val myTrace = FirebasePerformance.getInstance().newTrace("test_trace")
+        myTrace.start()
+
         val bundle = Bundle()
         bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "001")
         bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "App Open - Testing")
         firebaseAnalytics.logEvent(FirebaseAnalytics.Event.APP_OPEN, bundle)
+
+        myTrace.stop()
+        // TODO End of Testing
     }
 }
