7bf2cda303e595ce2806e843efa04f0e14cff7fe
willdavidking <will.david.king@gmail.com>
2019-08-24 10:01:56 -0600

    refactor/update-project-name -) Changed package name and directory names.

--- app/src/androidTest/java/com/mythicalcreaturesllc/breedingtracker/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mythicalcreaturesllc/breedingtracker/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.mythicalcreaturesllc.breedingandanimaltracking
+package com.mythcreatures.breedingtracker
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.mythicalcreaturesllc.breedingandanimaltracking", appContext.packageName)
+        assertEquals("com.mythcreatures.breedingtracker", appContext.packageName)
     }
 }
--- app/src/main/java/com/mythcreatures/breedingtracker/StartActivity.kt
+++ app/src/main/java/com/mythcreatures/breedingtracker/StartActivity.kt
@@ -1,4 +1,4 @@
-package com.mythicalcreaturesllc.breedingandanimaltracking
+package com.mythcreatures.breedingtracker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
